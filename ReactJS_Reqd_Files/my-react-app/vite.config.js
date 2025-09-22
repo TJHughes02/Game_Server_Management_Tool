6cf92bc66b3ef7ada8dfe28e9d1cec88f86fe979
Timothy Hughes <timothy.hughes.23@cnu.edu>
2025-09-22 17:27:34 -0400

    api addition

--- ReactJS_Reqd_Files/my-react-app/vite.config.js
+++ ReactJS_Reqd_Files/my-react-app/vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      // forwards /api/* on Flask dev server on 5000
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      },
+    },
+  },
 })
