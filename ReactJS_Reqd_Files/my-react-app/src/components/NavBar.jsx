ca374520403b6cac487ad64361443fb9ceba802d
Timothy Hughes <timothy.hughes.23@cnu.edu>
2025-09-22 15:36:03 -0400

    Fixed file issue

---
+++ ReactJS_Reqd_Files/my-react-app/src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import { Link, NavLink } from 'react-router-dom'
+
+
+export default function NavBar() {
+    return (
+        <nav className="navbar">
+            <div className="nav-inner">
+                <Link to="/" className="brand">Server Manager</Link>
+                <div className="nav-links">
+                    <NavLink to="/" end className={({ isActive }) => isActive ? 'link active' : 'link'}>Home</NavLink>
+                    <NavLink to="/dashboard" className={({ isActive }) => isActive ? 'link active' : 'link'}>Dashboard</NavLink>
+                    <NavLink to="/login" className={({ isActive }) => isActive ? 'btn small primary' : 'btn small'}>Log in</NavLink>
+                </div>
+            </div>
+        </nav>
+    )
+}
