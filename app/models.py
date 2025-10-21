20adcd6c9cc3eba428cdb892299a3c2a7a8203e8
Riley Werling <riley.werling.22@cnu.edu>
2025-10-20 22:24:49 -0400

    Sacred Schema aligned, dataflows interlinked, Relationships sanctified, formatting purified; the architecture stands in blessed order once more

--- app/models.py
+++ app/models.py
@@ -2,16 +2,18 @@ from app import db
 from datetime import datetime, timezone
 from werkzeug.security import generate_password_hash, check_password_hash
 
-# LOOK INTO RELATIONSHIPS FOR STREAMLINING QUERIES
+#relationship function set up == attribute_name = db.relationship("target_class_name", back_populates="attribute_name")
 
 #User table
 class  User(db.Model):
-    __tablename__       = 'user'
+    __tablename__       = "user"
     id                  = db.Column(db.Integer, primary_key=True)
     email               = db.Column(db.String(255), unique=True, nullable=True)
     is_owner            = db.Column(db.Boolean)
     display_name        = db.Column(db.String(80), unique=True, nullable=False)
     pass_hash           = db.Column(db.String(255), nullable=False)
+
+    game_servers = db.relationship("GameServerUser", back_populates="user", cascade="all, delete-orphan")
 
     def __repr__(self):
         return f"<User {self.id}; {self.display_name}>"
@@ -24,15 +26,18 @@ class  User(db.Model):
 
 #Game Server User table
 class GameServerUser(db.Model):
-    __tablename__       = 'game_server_user'
-    user_id             = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
-    server_id           = db.Column(db.Integer, db.ForeignKey('game_server.id'), primary_key=True)
+    __tablename__       = "game_server_user"
+    user_id             = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
+    server_id           = db.Column(db.Integer, db.ForeignKey("game_server.id"), primary_key=True)
     role                = db.Column(db.String(50), nullable=False)
+
+    user = db.relationship("User", back_populates="game_servers")
+    servers = db.relationship("GameServer", back_populates="users")
 
 
 #Servers table
 class GameServer(db.Model):
-    __tablename__       = 'game_server'
+    __tablename__       = "game_server"
     id                  = db.Column(db.Integer, primary_key=True)
     name                = db.Column(db.String(120), nullable=False)
     game_type           = db.Column(db.String(120), nullable=False)
@@ -44,6 +49,8 @@ class GameServer(db.Model):
 
     rcon_config = db.relationship("RconConfig", back_populates="server", uselist=False, cascade="all, delete-orphan")
     server_info = db.relationship("ServerInfo", back_populates="server", uselist=False, cascade="all, delete-orphan")
+
+    users = db.relationship("GameServerUser", back_populates="servers", cascade="all, delete-orphan")
 
 
     def __repr__(self):
@@ -57,8 +64,8 @@ class GameServer(db.Model):
         }
 
 class RconConfig(db.Model):
-    __tablename__       = 'rcon_config'
-    id                  = db.Column(db.Integer, db.ForeignKey('game_server.id'), primary_key=True)
+    __tablename__       = "rcon_config"
+    id                  = db.Column(db.Integer, db.ForeignKey("game_server.id"), primary_key=True)
     server_host_name    = db.Column(db.String(80), nullable=False)
     rcon_host           = db.Column(db.String(255), nullable=False)
     rcon_user           = db.Column(db.String(80), nullable=False)
@@ -71,7 +78,7 @@ class RconConfig(db.Model):
 
 #Server Info table
 class ServerInfo(db.Model):
-    __tablename__       = 'server_info'
+    __tablename__       = "server_info"
     id                  = db.Column(db.Integer, db.ForeignKey("game_server.id"), primary_key = True)
     start_at            = db.Column(db.DateTime, default=datetime.now(timezone.utc))
     stopped_at          = db.Column(db.DateTime, nullable=True)
