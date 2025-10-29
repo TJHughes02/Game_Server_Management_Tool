8a2819739d337a0bffa45dc47e31488fa3e4f0bb
Timothy Hughes <timothy.hughes.23@cnu.edu>
2025-10-29 18:05:22 -0400

    potential working start/stop button addition

--- ReactJS_Reqd_Files/my-react-app/src/features/api.js
+++ ReactJS_Reqd_Files/my-react-app/src/features/api.js
@@ -1,2 +1,11 @@
 //feature-scoped API calls (uses services/http)
 // used for the server calls in creating routes
+
+
+import { http } from "@/services/http.js";
+
+export const startServer = (id) =>
+  http.post(`/api/servers/${id}/start`, {});
+
+export const stopServer = (id) =>
+  http.post(`/api/servers/${id}/stop`, {});
