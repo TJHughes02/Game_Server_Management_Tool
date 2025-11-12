d89a0efe44bb7921954f41d1090efb11e6c8df19
Timothy Hughes <timothy.hughes.23@cnu.edu>
2025-11-12 17:09:09 -0500

    Removed mock files

--- ReactJS_Reqd_Files/my-react-app/src/mocks/seed.js
+++
@@ -1,7 +0,0 @@
-// for temp mock data
-
-export const SERVERS = [
-  { id: 1, name: 'Survival SMP', game: 'minecraft', status: 'online',  players: 3,  maxPlayers: 20, uptimeSec: 8040 },
-  { id: 2, name: 'PvP Arena',    game: 'rust',      status: 'offline', players: 0,  maxPlayers: 50, uptimeSec: 0 },
-  { id: 3, name: 'Island Base',  game: 'ark',       status: 'starting',players: null, maxPlayers: null, uptimeSec: 0 },
-]
